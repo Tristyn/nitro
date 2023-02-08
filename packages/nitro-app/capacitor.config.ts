f7dd783f6ee715a4fe0070ebaf470b3937e5ff2a
Tristyn <tristynstimpson@gmail.com>
2023-02-08 08:54:21 +0700

    Android, ios with nextjs capacitor

---
+++ packages/nitro-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.solananitro.app",
+  appName: "Solana Nitro",
+  webDir: "out",
+  bundledWebRuntime: false,
+};
+
+export default config;
